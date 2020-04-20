a7ebd97150ae8c02e50cf53b3160739ddec1c606
Marco Dinacci <mdinacci@apple.com>
2020-04-19 22:48:50 -0700

    Added doc in run_local_planner.sh

--- run_local_planners.sh
+++ run_local_planners.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# The script assumes that the lapkt docker image has been installed and a container running, e.g.
+# docker pull lapkt/lapkt-public
+# docker run -v /path/to/dungeon/files:/dungeon -it lapkt/lapkt-public
+# The -v option mount a local directory containing domain and problem files (and this script) 
+# into /dungeon in the container filesystem. The script must then be copied to the default directory
+# in the container which is ~/projects/lapkt/compiled_planners
+
 planners=(dfs_plus bfs_f siw siw_plus siw-then-bfsf ff)
 
 for planner in "${planners[@]}" 
