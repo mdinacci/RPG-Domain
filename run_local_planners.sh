663d4c0944c7cc202ff52201a032a64acfca4978
Marco Dinacci <mdinacci@apple.com>
2020-04-19 21:55:48 -0700

    Added script to run all local planners

--- run_local_planners.sh
+++ run_local_planners.sh
@@ -2,16 +2,19 @@
 
 planners=(dfs_plus bfs_f siw siw_plus siw-then-bfsf ff)
 
-$ for planner in "${planners[@]}" do 
-    printf "Using planner $planner\n" 
+for planner in "${planners[@]}" 
+do 
+    echo -e "Using planner $planner" 
 
     mkdir -p output/$planner
 
     for i in 1 2 3
     do
-        printf "./$planner --domain /dungeon/dungeon-domain.pddl --problem /dungeon/problem$i.pddl"
+        echo -e "./$planner --domain /dungeon/dungeon-domain.pddl --problem /dungeon/problem$i.pddl"
         ./$planner --domain /dungeon/dungeon-domain.pddl --problem /dungeon/problem$i.pddl --output output/$planner/$i.ipc
         mv execution.details output/$planner/execution_details_$i
     done
 done
+
+echo "All done."
 
